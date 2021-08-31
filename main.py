d3bfaec30700a70c84d53d571501dc06cfeaff6d
AzmainAhnaf <azmainahnaf2003@gmail.com>
2021-09-01 01:41:19 +0600

    Day, date and time is splited. Added tournament name

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ matches = soup.find_all('div', class_ = 'col-md-8 col-16')
 for match in matches:
     # match date and match time
     date_time = match.find('div', class_ = 'status').span.text
+    date_time = date_time.split(',')
+    
+    # weekly day of the match
+    day = date_time[0]
+
+    # date of the match
+    date = date_time[1]
+
+    # time of the match
+    time = date_time[-1]
     
     description = match.find('div', class_ = 'description').text
     comma_splited_description = description.split(',')
@@ -23,6 +33,9 @@ for match in matches:
     # match type (e.g. ODI, T20I, 6th match etc)
     match_type = (f"{match_type[0]} {match_type[1]}")
 
-    print(comma_splited_description)
+    # tournament name
+    tournament_name = (comma_splited_description[1])
+
+    print(time)
     print("------------------------------------------------------------------------------------\n")
 
