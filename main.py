3e0c09d2f957218d52fb8a012efd3fb99a184943
AzmainAhnaf <azmainahnaf2003@gmail.com>
2021-09-01 01:24:07 +0600

    The programme can find match type

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from bs4.element import Declaration
 import requests
 
 html_text = requests.get('https://www.espncricinfo.com/team/bangladesh-25/match-schedule-fixtures').text
@@ -8,11 +9,20 @@ soup = BeautifulSoup(html_text, 'lxml')
 matches = soup.find_all('div', class_ = 'col-md-8 col-16')
 
 for match in matches:
+    # match date and match time
     date_time = match.find('div', class_ = 'status').span.text
+    
     description = match.find('div', class_ = 'description').text
-    temp_description = description.split(',')
-    match_name = temp_description[0][5:]
+    comma_splited_description = description.split(',')
+    
+    # match vs
+    match_name = comma_splited_description[0][5:]
+    
+    match_type = comma_splited_description[2].split()
+    
+    # match type (e.g. ODI, T20I, 6th match etc)
+    match_type = (f"{match_type[0]} {match_type[1]}")
 
-    print(date_time, match_name)
+    print(comma_splited_description)
     print("------------------------------------------------------------------------------------\n")
 
